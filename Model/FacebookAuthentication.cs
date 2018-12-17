440311f578deddadd42a96da972041566874cfd2
Liad Pasker <liadpa@mta.ac.il>
2018-12-17 20:37:26 +0200

    1

--- Model/FacebookAuthentication.cs
+++ Model/FacebookAuthentication.cs
@@ -64,24 +64,4 @@ namespace Model
             }
         }
     }
-
-    /* TO REMOVE
-         public class FacebookAuthentication
-    {
-        public User LoggedInUser { get; set; }
-
-        public string Login()
-        {
-            LoginResult result = FacebookService.Login("295427534630566", "public_profile", "email", "user_friends", "user_photos", "user_birthday", "user_likes", "manage_pages", "user_events", "user_hometown", "user_posts", "user_tagged_places", "user_location");
-            LoggedInUser = result.LoggedInUser;
-            return result.AccessToken;
-        }
-
-        public void AutoLogin(string i_LastAccessToken)
-        {
-            LoginResult loginResult = FacebookService.Connect(i_LastAccessToken);
-            LoggedInUser = loginResult.LoggedInUser;
-        }
-    }
-     */
 }
